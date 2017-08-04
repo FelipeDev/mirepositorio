a72b8b2d7cf5c07a8ff490cfb192795b6f1e4910
antzelada <antzelada@gmail.com>
2017-08-04 11:05:47 -0400

    carpeta clima

---
+++ clima/weather.js
@@ -0,0 +1,10 @@
+function enviarPeticion(){
+
+	var xhttp = new XMLHttpRequest();
+    xhttp.open("response", "http://api.openweathermap.org/data/2.5/forecast?q=Santiago&units=metric&appid=d2bc8baa667084ecf883f6c22d4e15be", false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+    console.log(response.cod);
+    //http://api.openweathermap.org/data/2.5/weather?q=London&appid=d2bc8baa667084ecf883f6c22d4e15be
+}
